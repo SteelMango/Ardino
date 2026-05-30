b14fa91c7b52518ea0c8d5f55953930d5bb246f5
farquahp <p.farquharson@lancaster.ac.uk>
2026-05-30 18:12:45 +0100

    Added Three LEDS and moved files to the correct directory.

---
+++ PlatformIO/Projects/Led_Thermostat/src/main.cpp
@@ -0,0 +1,88 @@
+#include <Arduino.h>
+#include <DHT.h>
+
+
+
+// Global Variables
+int  error_ledPin = 2;
+#define DHTPIN 21
+#define DHTTYPE DHT11
+
+// Temperature based Variables
+int hot_led_pin = 4;
+int cold_led_pin = 5;
+int tepid_led_pin = 19;
+
+int temp_results;
+DHT dht(DHTPIN, DHTTYPE);
+float temp = dht.readTemperature();
+
+
+
+void error_led(){
+  digitalWrite(error_ledPin, HIGH);
+  delay(250);
+  digitalWrite(error_ledPin, LOW);
+  delay(250);
+
+}
+
+int create_temp_result(int temp){
+  if (temp >= 11 && temp <= 21) {   // Tepid
+    return temp_results = 1; 
+  } else if ( temp > 21) {    // hot
+    return temp_results = 2;
+  } else {
+    return temp_results = 3;
+  }
+}
+
+void check_temp_leds(){
+  digitalWrite(hot_led_pin, HIGH);
+  digitalWrite(tepid_led_pin, HIGH);
+  digitalWrite(cold_led_pin, HIGH);
+}
+
+
+
+
+void setup() {
+  pinMode(error_ledPin, OUTPUT);
+
+  // Temurature Leds
+  pinMode(hot_led_pin, OUTPUT);
+  pinMode(cold_led_pin, OUTPUT);
+  pinMode(tepid_led_pin, OUTPUT);
+
+  // LED Dimming
+  ledcSetup(0, 5000, 8);
+  ledcAttachPin(13, 0);
+
+
+
+
+  Serial.begin(115200);     
+  dht.begin();
+  delay(2000);
+ 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  ledcWrite(0, 10);   // On LED
+  error_led();
+  check_temp_leds();
+  float temp = dht.readTemperature();      
+  float humidity = dht.readHumidity();     
+
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+  Serial.println(" °C");
+
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.println(" %");
+
+  delay(2000);
+}
+
