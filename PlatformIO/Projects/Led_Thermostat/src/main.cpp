2c6e98c0c1069c105c3ea063ba3f22af88e5dbb6
farquahp <p.farquharson@lancaster.ac.uk>
2026-05-30 21:15:36 +0100

    Working LED Temp detector

--- PlatformIO/Projects/Led_Thermostat/src/main.cpp
+++ PlatformIO/Projects/Led_Thermostat/src/main.cpp
@@ -12,12 +12,11 @@ int on_ledPin = 12;
 // Temperature based Variables
 int hot_led_pin = 4;
 int cold_led_pin = 5;
-int tepid_led_pin = 19;
+int tepid_led_pin = 22;
 
-int temp_results;
+
 DHT dht(DHTPIN, DHTTYPE);
-float temp = dht.readTemperature();
-
+int temp_results;
 
 
 void error_led(){   // So far the led is always on, add future code to change if nan
@@ -38,11 +37,21 @@ int create_temp_result(int temp){
   }
 }
 
-void check_temp_leds(){
-  digitalWrite(hot_led_pin, HIGH);
-  digitalWrite(tepid_led_pin, HIGH);
-  digitalWrite(cold_led_pin, HIGH);
+void turn_temp_led_on(int temp_results){
+  digitalWrite(hot_led_pin, LOW);
+  digitalWrite(tepid_led_pin, LOW);
+  digitalWrite(cold_led_pin, LOW);
+
+  if (temp_results == 1){
+    analogWrite(tepid_led_pin, 5);
+  } else if (temp_results  == 2){
+    analogWrite(hot_led_pin, 5);
+  } else if (temp_results == 3){
+    analogWrite(cold_led_pin, 5);
+  }
+
 }
+
 
 
 
@@ -71,19 +80,19 @@ void setup() {
 void loop() {
   // LEDs
   error_led();
-  check_temp_leds();
 
   // Dimming the LEDS
   analogWrite(on_ledPin, 5);
-  analogWrite(hot_led_pin, 5);
-  analogWrite(tepid_led_pin, 5);
-  analogWrite(cold_led_pin, 5);
-
-
-
-
   float temp = dht.readTemperature();      
-  float humidity = dht.readHumidity();     
+  float humidity = dht.readHumidity();   
+
+  int result = create_temp_result((int)temp);
+  turn_temp_led_on(result);
+
+
+
+
+    
 
   Serial.print("Temperature: ");
   Serial.print(temp);
