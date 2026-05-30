2a14e8eb1281752dea47c26d8a1776f1be411f2b
farquahp <p.farquharson@lancaster.ac.uk>
2026-05-30 21:27:41 +0100

    Cleaned the code

--- PlatformIO/Projects/Led_Thermostat/src/main.cpp
+++ PlatformIO/Projects/Led_Thermostat/src/main.cpp
@@ -1,8 +1,6 @@
 #include <Arduino.h>
 #include <DHT.h>
 
-
-
 // Global Variables
 int  error_ledPin = 2;
 int on_ledPin = 12;
@@ -13,7 +11,6 @@ int on_ledPin = 12;
 int hot_led_pin = 4;
 int cold_led_pin = 5;
 int tepid_led_pin = 22;
-
 
 DHT dht(DHTPIN, DHTTYPE);
 int temp_results;
@@ -27,13 +24,15 @@ void error_led(){   // So far the led is always on, add future code to change if
 
 }
 
-int create_temp_result(int temp){
-  if (temp >= 11 && temp <= 21) {   // Tepid
-    return temp_results = 1; 
-  } else if ( temp > 21) {    // hot
-    return temp_results = 2;
+int create_temp_result(float temp) {
+  if (isnan(temp)) {    // Checks for nan 
+    return 4;                
+  } else if (temp >= 11 && temp <= 21) {
+    return 1;                
+  } else if (temp > 21) {
+    return 2;                
   } else {
-    return temp_results = 3;
+    return 3;                 
   }
 }
 
@@ -48,13 +47,10 @@ void turn_temp_led_on(int temp_results){
     analogWrite(hot_led_pin, 5);
   } else if (temp_results == 3){
     analogWrite(cold_led_pin, 5);
+  } else if ( temp_results == 4){
+    error_led();
   }
-
 }
-
-
-
-
 
 void setup() {
   pinMode(error_ledPin, OUTPUT);
@@ -67,32 +63,19 @@ void setup() {
   // Other Communication Leds (On_led and Error_Led)
   pinMode(on_ledPin, OUTPUT);
 
- 
-
-
-
   Serial.begin(115200);     
   dht.begin();
   delay(2000);
- 
 }
 
 void loop() {
-  // LEDs
-  error_led();
-
   // Dimming the LEDS
   analogWrite(on_ledPin, 5);
   float temp = dht.readTemperature();      
   float humidity = dht.readHumidity();   
 
-  int result = create_temp_result((int)temp);
+  int result = create_temp_result(temp);
   turn_temp_led_on(result);
-
-
-
-
-    
 
   Serial.print("Temperature: ");
   Serial.print(temp);
@@ -102,6 +85,6 @@ void loop() {
   Serial.print(humidity);
   Serial.println(" %");
 
-  delay(2000);
+  delay(1000);
 }
 
