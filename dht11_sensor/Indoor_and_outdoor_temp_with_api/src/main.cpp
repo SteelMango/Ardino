5371a949a8eb3c62b607dbc1a336b0066d2ac276
PenelopeF <Penelope.Farquharson@protonmail.com>
2026-06-05 23:16:34 +0100

    Added function for anti-ghosting for the leds multiplexing

--- dht11_sensor/Indoor_and_outdoor_temp_with_api/src/main.cpp
+++ dht11_sensor/Indoor_and_outdoor_temp_with_api/src/main.cpp
@@ -7,25 +7,17 @@ const int latchPin = 19;
 // D1, D2, D3, D4 pins should be paired to the microcontroller directly.
 const int dig4 = 4;
 const int dig1 = 2;
-
-byte segment_dig4[] = {
-  0b01000000,
-  0b00100000,
-  0b00010000,
-  0b00001000,
-  0b00000100,
-};
-
-byte combined_dig4 = segment_dig4[0] | segment_dig4[1] | segment_dig4[2] | segment_dig4[3] | segment_dig4[4];
+const int dig2 = 15;
+const int dig3 = 18;
 
 // QA=bit7, QB=bit6, QC=bit5, QD=bit4, QE=bit3, QF=bit2, QG=bit1
-// byte segA = 0b10000000;
-// byte segB = 0b01000000;
-// byte segC = 0b00100000;
-// byte segD = 0b00010000;
-// byte segE = 0b00001000;
-// byte segF = 0b00000100;
-// byte segG = 0b00000010;
+byte segA = 0b10000000;
+byte segB = 0b01000000;
+byte segC = 0b00100000;
+byte segD = 0b00010000;
+byte segE = 0b00001000;
+byte segF = 0b00000100;
+byte segG = 0b00000010;
 
 byte all_On = 0b11111111;
 byte all_Off = 0b00000000;
@@ -40,6 +32,23 @@ void iterate_all_pins() {
   }
 }
 
+// Should prevent ghosting
+void turn_off_all_digits() {
+  digitalWrite(dig1, HIGH);
+  digitalWrite(dig2, HIGH);
+  digitalWrite(dig3, HIGH);
+  digitalWrite(dig4, HIGH);
+}
+
+void show_digit(int digitPin, byte segment) {
+  turn_off_all_digits();
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, MSBFIRST, segment);
+  digitalWrite(latchPin, HIGH);
+  digitalWrite(digitPin, LOW);
+  delay(1);
+}
+
 
 void setup() {
   // Shift Register 
@@ -47,20 +56,14 @@ void setup() {
   pinMode(clockPin, OUTPUT);
   pinMode(latchPin, OUTPUT);
 
-  pinMode(dig4, OUTPUT);
-  digitalWrite(dig4, LOW);
-
   pinMode(dig1, OUTPUT);
-  digitalWrite(dig1, LOW);
+  pinMode(dig2, OUTPUT);
+  pinMode(dig3, OUTPUT);
+  pinMode(dig4, OUTPUT);
+  
+  turn_off_all_digits();
 }
 
 void loop() {
-  digitalWrite(latchPin, LOW);
-  shiftOut(dataPin, clockPin ,MSBFIRST, all_On);
-  digitalWrite(latchPin, HIGH);
-  delay(100);
-
-
-  // iterate_all_pins();
-
+  show_digit(dig1, segA);
 }
